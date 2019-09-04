79c80e9b475fbfb9aec82c6c5be36276b517b9be
neoxhh <neoxhh@chinawebeye.com>
2019-09-04 11:12:42 +0800

    添加批量换字体工具

---
+++ GameFramework/Assets/GFrame/Editor/PlayerPrefTools.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlayerPrefTools
+{
+    [MenuItem("PlayerPref Tools/Clear Saved Data")]
+	static public void ClearSavedData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
