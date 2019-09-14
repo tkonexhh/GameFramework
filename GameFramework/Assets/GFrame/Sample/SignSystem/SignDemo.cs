6d80f93d0957cac75a3f6c06901c58d20dde3fbd
neoxhh <neoxhh@chinawebeye.com>
2019-09-14 18:04:50 +0800

    SignSystem

---
+++ GameFramework/Assets/GFrame/Sample/SignSystem/SignDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFrame.Sample
+{
+
+    public class SignDemo : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SignSystem.S.InitSignSystem();
+        }
+
+
+        private void Start()
+        {
+            Debug.LogError(SignSystem.S.weekSign.signAbleIndex);
+            SignSystem.S.weekSign.Sign();
+            Debug.LogError(SignSystem.S.weekSign.signAbleIndex);
+        }
+    }
+}
+
+
+
+
---
+++ GameFramework/Assets/Scripts/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var str = DateTime.Now.ToShortDateString();
+        Debug.LogError(str);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
